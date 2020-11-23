c07581cc2f65609dc8db172fbc405058b27f8064
ldacunha <lucas@DESKTOP-R65CCSO.localdomain>
2020-11-23 11:50:00 +0100

    maj

--- app.js
+++ app.js
@@ -6,6 +6,10 @@
  *                                                                      *
  ************************************************************************/
 document.addEventListener("DOMContentLoaded", function (_e) {
+	
+	if('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('./sw.js');
+	};
     
     /******************************************************************
             Fonctions à compléter dans la dernière partie du TP 
